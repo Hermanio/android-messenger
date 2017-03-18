caa86a77bd163525a43a1fb7e8686107f42a1b92
Herman Õunapuu <hermanounapuu@gmail.com>
2017-03-18 15:49:08 +0200

    testing

--- Messenger/app/src/main/java/ee/ounapuu/herman/messenger/MainActivity.java
+++ Messenger/app/src/main/java/ee/ounapuu/herman/messenger/MainActivity.java
@@ -32,7 +32,6 @@ public class MainActivity extends AppCompatActivity {
 
     };
 
-    //test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
