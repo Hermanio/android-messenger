1005e14c077b65f84cd706c22bb1a76e833ce881
Herman Õunapuu <hermanounapuu@gmail.com>
2017-03-19 16:32:09 +0200

    changes

--- Messenger/app/src/main/java/ee/ounapuu/herman/messenger/LoginActivity.java
+++ Messenger/app/src/main/java/ee/ounapuu/herman/messenger/LoginActivity.java
@@ -1,8 +1,11 @@
 package ee.ounapuu.herman.messenger;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Toast;
 
 /**
  * Created by toks on 3/19/17.
@@ -14,5 +17,19 @@ public class LoginActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+    }
+
+    public void login(View view) {
+
+        Toast.makeText(this, "Add actual login", Toast.LENGTH_SHORT).show();
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+
+    public void startRegistration(View view) {
+        Intent i = new Intent(this, RegisterActivity.class);
+        startActivity(i);
+        finish();
     }
 }
--- Messenger/app/src/main/java/ee/ounapuu/herman/messenger/RegisterActivity.java
+++ Messenger/app/src/main/java/ee/ounapuu/herman/messenger/RegisterActivity.java
@@ -1,8 +1,11 @@
 package ee.ounapuu.herman.messenger;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Toast;
 
 /**
  * Created by toks on 3/19/17.
@@ -14,5 +17,12 @@ public class RegisterActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+    }
+
+    public void register(View view) {
+        Toast.makeText(this, "todo: add actual registration", Toast.LENGTH_SHORT).show();
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+        finish();
     }
 }
