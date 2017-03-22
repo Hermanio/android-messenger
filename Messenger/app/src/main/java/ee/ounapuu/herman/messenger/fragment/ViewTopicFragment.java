0bfe4f36a5bc90ceae62da099ed90dd5a0d0f07d
Herman Õunapuu <hermanounapuu@gmail.com>
2017-03-22 09:01:47 +0200

    Added example listview with simple objects

--- Messenger/app/src/main/java/ee/ounapuu/herman/messenger/fragment/ViewTopicFragment.java
+++ Messenger/app/src/main/java/ee/ounapuu/herman/messenger/fragment/ViewTopicFragment.java
@@ -1,10 +1,15 @@
 package ee.ounapuu.herman.messenger.fragment;
 
 import android.app.Fragment;
+import android.app.ListFragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.List;
 
 import ee.ounapuu.herman.messenger.R;
 
@@ -13,6 +18,31 @@ import ee.ounapuu.herman.messenger.R;
  */
 
 public class ViewTopicFragment extends Fragment {
+    String[] itemname ={
+            "Safari",
+            "Camera",
+            "Global",
+            "FireFox",
+            "UC Browser",
+            "Android Folder",
+            "VLC Player",
+            "Safari",
+            "Camera",
+            "Global",
+            "FireFox",
+            "UC Browser",
+            "Android Folder",
+            "VLC Player",
+            "Safari",
+            "Camera",
+            "Global",
+            "FireFox",
+            "UC Browser",
+            "Android Folder",
+            "VLC Player",
+            "Cold War"
+    };
+
     public static ViewTopicFragment newInstance() {
         ViewTopicFragment fragment = new ViewTopicFragment();
         return fragment;
@@ -26,6 +56,10 @@ public class ViewTopicFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_view_topic, container, false);
+        View view = inflater.inflate(R.layout.fragment_view_topic, container, false);
+        ListView list = (ListView) view.findViewById(R.id.customlist);
+
+        list.setAdapter(new ArrayAdapter<String>(getActivity(), R.layout.custom_list, R.id.Itemname,itemname));
+        return view;
     }
 }
